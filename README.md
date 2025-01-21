f852c0fba5f73e9a271f6490a8d0249587caef4f
Noah4Puppy <chenxuanxinsp@outlook.com>
2025-01-21 23:44:41 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # LogParserX
 A well-performance log parser model for rules generation and parsing task focusing on untagged log information. Specifically for 2025th West Lake Digitalsecurity Conference.
+
+----
+
+### Model
+
+- Qwen-2.5-72B-Instruct (about 148GB): https://huggingface.co/Qwen/Qwen2.5-72B-Instruct/tree/main
+
+- Qwen-2.5-7B-Instruct (about 16GB): https://huggingface.co/Qwen/Qwen2.5-7B-Instruct/tree/main
+
+(1) A little try: deployment for Qwen-2.5-7B for testing
+
+For some easy testing, but actually you should interact through API Key.
+
+(2) API Key Link: 
+
+官方正版：https://help.aliyun.com/zh/model-studio/billing-for-model-studio
+
+中转1折：https://wcode.net/qwen-llm-api
+
+
+
---
+++ src/demo.py
@@ -0,0 +1,37 @@
+# my demo scripts
+import openai
+# Qwen-2.5-72b-instruct
+def get_chat_completions(messages, api_key, base_url, use_llm_model):
+    """
+    使用 openai 接口调用大模型。
+    """
+    client = openai.OpenAI(api_key=api_key, base_url=base_url)
+
+    response = client.chat.completions.create(model=use_llm_model, messages=messages)
+
+    return response
+
+# usage example
+messages = [
+    {
+      "role": "system",
+      "content": "You are a helpful assistant."
+    },
+    {
+      "role": "user",
+      "content": "你好"
+    }
+  ]
+
+api_key = "sk-63.h3A5gkOyaHkT8W9wPKtu28gqEzhkpR5X53NKFyzX9eq5dIEH"
+base_url = "https://wcode.net/api/gpt/v1"
+use_llm_model = "qwen2.5-72b-instruct"
+response = get_chat_completions(messages, api_key, base_url, use_llm_model)
+reply_content = response.choices[0].message.content
+print(reply_content)
+
+# response = ChatCompletion(id='chatcmpl-e3086a05-a198-96d3-9dab-8543c566134e', 
+#                choices=[Choice(finish_reason='stop', index=0, logprobs=None, 
+#             message=ChatCompletionMessage(content='你好！有什么可以帮助你的吗？', role='assistant', function_call=None, tool_calls=None))], created=1737474028, model='qwen2.5-72b-instruct', object='chat.completion', 
+#             system_fingerprint=None, 
+#                usage=CompletionUsage(completion_tokens=7, prompt_tokens=20, total_tokens=27))
